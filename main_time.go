96147706b055eb60ac067e13ae49543dd468447c
nikitamirzani323 <nikitamirzani323@gmail.com>
2021-09-15 01:18:26 +0700

    TIME + REGEX

---
+++ main_time.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+
+	fmt.Println(now)
+	fmt.Println(now.Year())
+	fmt.Println(now.Month())
+	fmt.Println(now.Day())
+	fmt.Println(now.Hour())
+	fmt.Println(now.Minute())
+	fmt.Println(now.Second())
+	fmt.Println(now.Nanosecond())
+
+	utc := time.Date(2020, 10, 10, 10, 10, 10, 10, time.UTC)
+	fmt.Println(utc)
+
+	layout := time.RFC3339
+	parse, _ := time.Parse(layout, "2020-10-01")
+	fmt.Println(parse)
+}
